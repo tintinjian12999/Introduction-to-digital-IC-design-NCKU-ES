7e5fc25a95fdbfe88eb3f2de9b3b7cd8da9b353f
tintinjian12999 <118806011+tintinjian12999@users.noreply.github.com>
2022-12-05 22:17:47 +0800

    Add files via upload

--- counter.v
+++ counter.v
@@ -2,92 +2,79 @@ module counter(out, clk, rst, in);
 	input clk;
 	input rst; 
 	input [1:0] in;
-	output reg [3:0] out;
+	output reg [3:0]out;
 	reg [3:0]next_out;
-	reg state;
-	reg repeat1;
-	reg plus;
-//Next state logic
-always@(posedge clk)
-begin
-    if(out == 0)
-    begin
-        if(repeat1 == 0) next_out = out + in; 
-        else
-        begin
-            next_out = 0;
-            repeat1 = 0;
-        end
-    end
-    
-    else if(out == 15)
-    begin
-        if(repeat1 == 0) next_out = out - in; 
-        else
-        begin
-            next_out = 15;
-            repeat1 = 0;
-        end    
-    end
-    
-    else
-    begin
-        if(plus == 1)
-        begin
-            if((out + in)>15)
-            begin
-                next_out = 15;
-                repeat1 = 0;
-                plus = 0;
-            end
-            else if ((out + in)==15)
-            begin
-            //if out + in equal to 15, the output must repeat once
-                next_out = 15;
-                repeat1 = 1;
-                plus = 0;
-            end
-            else next_out = out + in;
-        end
-        else
-        begin
-            if((out < in) )
-            begin
-            // if out < in than the resulting output will lower than 0
-                next_out = 0;
-                repeat1 = 0;
-                plus = 1;
-            end
-            else if ((out - in)==0)
-            begin
-            //if out - in equal to 0, the output must repeat once
-                next_out = 0;
-                repeat1 = 1;
-                plus = 1;
-            end
-            else next_out = out - in;
-        end
-    end
-    
-end
-//State registor
+	reg [2:0]state;
+	reg [2:0]next_state;
+  reg overflow;
+//state register
 always@(posedge clk or posedge rst)
 begin
-    if(rst) state = 0;
-    else state = 1;
+    if(rst == 1) state = 3;
+    else
+    begin
+      state = next_state;
+      case(state)
+      0:  next_out = out;
+      1:
+      begin
+        if(out + in > 15)
+        begin
+         next_out = 15;
+         overflow = 1;
+        end
+        else
+        begin
+          next_out = out + in;
+          overflow = 0;
+        end
+      end
+      2:
+      begin
+        if(in > out)
+        begin
+          overflow = 1;
+          next_out = 0;
+        end
+        else
+        begin
+         next_out = out - in;
+         overflow = 0;
+        end
+      end
+      default:  next_out = 0;
+      endcase
+    end
 end
-//Output logic
+//next state logic
 always@(*)
 begin
-    if(state == 0)
-    begin
-    //initial states, plus=1 --> plus, plus = 0--> minus
-        out = 0;
-        repeat1 = 0;
-        plus = 1;
-    end
-    else out = next_out;
+    case(state)
+    // state = 0 -> hold, state = 1 -> plus, state = 2 -> minus, state = 3 -> reset
+     0:begin
+        if(next_out == 0)    next_state = 1;
+        else if(next_out == 15)    next_state = 2;
+        else next_state = 0;
+       end
+     1:begin
+        if(next_out == 15 && overflow == 0)  next_state = 0;
+        else if (next_out == 15 && overflow == 1)  next_state = 2;
+        else next_state = 1;
+       end
+     2:begin
+        if(next_out == 0 && overflow == 0) next_state = 0;
+        else if(next_out == 0 && overflow == 1)  next_state = 1;
+        else next_state = 2;
+       end
+     3:begin
+        next_state = 1;
+       end
+     endcase
 end
-
+//output logic
+always@(*)
+begin
+  if(state == 3)  out = 0;
+  else out = next_out;
+end
 endmodule
-
